e4efe07b6157a4daf783fbdf034ea51ab32785b2
Felip3ms <felipesilveiramonteiro@gmail.com>
2024-11-14 17:25:21 -0300

    Mudanças feitas: Implementação de Links no texto e Imagens ao decorrer do texto

---
+++ template/historia-poo/historia-poo.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    function activateCarousel() {
+        // Verifica se a tela é menor ou igual a 768px (mobile)
+        if ($(window).width() <= 768 && $('.navbar-mobile').length === 0) {
+            // Cria uma nova navbar móvel com carrossel apenas uma vez
+            $('body').prepend(`
+                <div class="navbar-mobile">
+                    <div class="nav-carousel">
+                        <!--<a href="/src/template/home-page/index.html">Apresentação</a>-->
+                        <a href="template/historia-dart/historia-dart.html">Historia do Dart</a>
+                        <a href="template/atributos-dart/atr-dart.html">Atributos do Dart</a>
+                        <a href="#">Atributos do Paradigma</a>
+                    </div>
+                </div>
+            `);
+
+            // Ativa o carrossel na navbar mobile com setas nas laterais
+            $('.nav-carousel').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+                arrows: true,
+                prevArrow: "<button type='button' class='slick-prev'>&#9664;</button>",
+                nextArrow: "<button type='button' class='slick-next'>&#9654;</button>"
+            });
+        }
+    }
+
+    // Ativa o carrossel ao carregar a página, se necessário
+    activateCarousel();
+
+    // Verifica a largura da janela ao redimensionar
+    $(window).resize(function() {
+        // Remove a navbar mobile se a tela for maior que 768px (desktop)
+        if ($(window).width() > 768 && $('.navbar-mobile').length > 0) {
+            $('.navbar-mobile').remove();
+        } else {
+            activateCarousel(); // Reativa o carrossel se for redimensionado para uma tela menor
+        }
+    });
+});
